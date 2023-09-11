b78ef0cc2d37724247827ad2a1e8106c9b784741
Bamboogamer <bamboogamer178@gmail.com>
2023-09-11 15:17:59 -0600

    Adding PDF and gitignore for login file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def test():
-    print("Hello")
+def getLogin():
+    loginInfo = open("login.txt", "r") \
+                 .readline() \
+                 .split(";")
+
+    return loginInfo[0], loginInfo[1]
 
 
 if __name__ == '__main__':
-    pass
+    print(getLogin())
