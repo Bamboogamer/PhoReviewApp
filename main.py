5ab8d8fa585141e732457a69f58cd26b2aceceeb
Bamboogamer <bamboogamer178@gmail.com>
2023-09-23 13:33:22 -0600

    Taking from Example code for set up

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ def getLogin():
                  .readline() \
                  .split(";")
 
-    return loginInfo[0], loginInfo[1]
+    return loginInfo[0], loginInfo[1], loginInfo[2]
+
+print("Starting ...")
+
+# Initialize client. Uses the API token in the environment variable "SMARTSHEET_ACCESS_TOKEN"
+smart = smartsheet.Smartsheet(access_token=getLogin()[2])
+# Make sure we don't miss any error
+smart.errors_as_exceptions(True)
+
+# Log all calls
+logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
 
 
 if __name__ == '__main__':
-    print(getLogin())
+    pass
